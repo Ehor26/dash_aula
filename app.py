dc3f013521031dce74be402c37486d8c11fede6b
Estevam H. O. Rizek <32528638+Ehor26@users.noreply.github.com>
2023-09-09 17:44:09 -0300

    Update app.py

--- app.py
+++ app.py
@@ -99,4 +99,4 @@ def render_graphs(cities, main_variable):
 
 # =========  Run server  =========== #
 if __name__ == "__main__":
-    app.run_server(debug=False)
+    app.run_server(debug=False, host="0.0.0.0", port=80)
